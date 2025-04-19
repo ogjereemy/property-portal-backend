0d876b4a69620fa64234c5cd0ae8906e7c75b2b2
ogola jeremy <ogolajeremy601@gmail.com>
2025-04-19 21:18:16 +0300

    Prepare frontend for Vercel deployment

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 // CORS configuration
 const allowedOrigins = [
   'http://localhost:3001',
-  'https://property-portal-web.vercel.app/',
+  'https://property-portal-web.vercel.app',
 ];
 
 app.use(cors({
